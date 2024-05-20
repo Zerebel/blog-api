0d34769adf5faabaac44b54ea1169d1c8c6e156a
Zerebel <86872654+Zerebel@users.noreply.github.com>
2024-05-20 23:39:05 +0100

    chore: Add proxy configuration for API server

--- api/vite.config.js
+++ api/vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // a proxy for the API
+  server: {
+    proxy: {
+      "/api": "http://localhost:3000",
+    },
+  },
   plugins: [react()],
-})
+});
